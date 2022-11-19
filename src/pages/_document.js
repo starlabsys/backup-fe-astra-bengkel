c1dc34b85dbb50b5683e3458c0eddd1990791a1b
Medianto Pratama Pentadakosta <pentadakosta99@student.untan.ac.id>
2022-11-20 02:51:17 +0700

    set header

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,6 @@
 import Document, {Html, Head, Main, NextScript} from 'next/document'
 
+
 class MyDocument extends Document {
     render() {
         return (
@@ -7,13 +8,20 @@ class MyDocument extends Document {
                 <Head>
                     {/* eslint-disable-next-line @next/next/no-title-in-document-head */}
                     <title>Astra Honda Pontianak</title>
-                    <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
+                    <meta name = "description" content = {'Astra Honda Pontianak'}/>
+                    <meta name = "keywords" content = {'Astra Honda Pontianak'}/>
+                    <meta name = "viewport" content = {'width=device-width, initial-scale=1.0'}/>
+                    <link rel = "icon" href = "../../public/img/logo-astra-only.png"/>
+                    <link rel = "preconnect" href = "https://fonts.googleapis.com"/>
+                    <link rel = "preconnect" href = "https://fonts.gstatic.com" crossOrigin = ""/>
                     <link
-                        href="https://fonts.googleapis.com/css2?family=Archivo:wght@400;600;700&family=Pacifico&family=Roboto&display=swap"
-                        rel="stylesheet"/>
-                    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-                          rel="stylesheet"/>
+                        href = "https://fonts.googleapis.com/css2?family=Archivo:wght@400;600;700&family=Pacifico&family=Roboto&display=swap"
+                        rel = "stylesheet"/>
+                    <link href = "https://fonts.googleapis.com/icon?family=Material+Icons"
+                          rel = "stylesheet"/>
+                    {/*<link rel = "shortcut icon" href = {require('../../public/img/logo3.png')}/>*/}
+                    {/*<link rel = "apple-touch-icon"*/}
+                    {/*      href = {require('../../public/img/logo3.png')}/>*/}
                 </Head>
                 <body>
                 <Main/>

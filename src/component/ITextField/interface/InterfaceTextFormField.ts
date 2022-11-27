5d83bf261fe2af48cfcc0ffeb7a5efa7a4c01943
Medianto Pratama Pentadakosta <pentadakosta99@student.untan.ac.id>
2022-11-27 16:50:37 +0700

    text field

--- src/component/ITextField/interface/InterfaceTextFormField.ts
+++ src/component/ITextField/interface/InterfaceTextFormField.ts
@@ -3,70 +3,71 @@ import { InterfaceListSelectOption } from "./InterfaceListSelectOption";
 
 
 export interface InterfaceIndicator {
-    label : string;
-    value : string;
-    onChange : ChangeEventHandler<HTMLInputElement>;
+    label: string;
+    value: string;
+    onChange: ChangeEventHandler<HTMLInputElement>;
 }
 
 export interface InterfaceRadio {
-    label? : string;
-    value : string
-    value1? : string;
-    value2? : string;
-    onClickValue1? : MouseEventHandler<HTMLDivElement>
-    onClickValue2? : MouseEventHandler<HTMLDivElement>
+    label?: string;
+    value: string
+    value1?: string;
+    value2?: string;
+    onClickValue1?: MouseEventHandler<HTMLDivElement>
+    onClickValue2?: MouseEventHandler<HTMLDivElement>
 }
 
 
 export interface InterfaceTextFieldRounded {
-    placeholder : string;
-    autoComplete? : string;
-    hidden? : boolean;
-    type : HTMLInputTypeAttribute
-    onChange : ChangeEventHandler<HTMLInputElement>
-    required? : boolean;
-    name? : string
-    onEnter : "search" | "enter" | "done" | "go" | "next" | "previous" | "send" | undefined;
+    placeholder: string;
+    autoComplete?: string;
+    hidden?: boolean;
+    type: HTMLInputTypeAttribute
+    onChange: ChangeEventHandler<HTMLInputElement>
+    required?: boolean;
+    name?: string
+    onEnter: "search" | "enter" | "done" | "go" | "next" | "previous" | "send" | undefined;
 }
 
 
 export interface InterfaceTextFieldDefault {
-    type : HTMLInputTypeAttribute;
-    disabled? : boolean;
-    required? : boolean;
-    onChange? : ChangeEventHandler<HTMLInputElement>
-    error? : boolean;
-    placeholder? : string;
-    name? : string
-    label : string;
-    labelColor? : string;
-    backgroundLabel? : string;
-    onEnter : "search" | "enter" | "done" | "go" | "next" | "previous" | "send" | undefined;
+    type: HTMLInputTypeAttribute;
+    disabled?: boolean;
+    required?: boolean;
+    onChange?: ChangeEventHandler<HTMLInputElement>
+    error?: boolean;
+    placeholder?: string;
+    name?: string
+    label: string;
+    labelColor?: string;
+    backgroundLabel?: string;
+    onEnter: "search" | "enter" | "done" | "go" | "next" | "previous" | "send" | undefined;
+    errorMessages?: string;
 }
 
 export interface InterfaceSelectOption {
-    label? : string;
-    placeHolder? : string;
-    type? : "email" | "search" | "tel" | "text" | "url" | "none" | "numeric" | "decimal" | undefined
-    onSelect? : ChangeEventHandler<HTMLSelectElement>
-    listOption : InterfaceListSelectOption[]
-    name? : string
+    label?: string;
+    placeHolder?: string;
+    type?: "email" | "search" | "tel" | "text" | "url" | "none" | "numeric" | "decimal" | undefined
+    onSelect?: ChangeEventHandler<HTMLSelectElement>
+    listOption: InterfaceListSelectOption[]
+    name?: string
 }
 
 
 export interface InterfaceRadioSingle {
-    label? : string;
-    value1? : string;
-    status : boolean;
-    setStatus : () => void
+    label?: string;
+    value1?: string;
+    status: boolean;
+    setStatus: () => void
 }
 
 export interface InterfaceTextArea {
-    label? : string;
-    name? : string;
-    placeHolder? : string;
-    type? : HTMLInputTypeAttribute
-    onChange? : ChangeEventHandler<HTMLTextAreaElement>
-    error? : true | undefined
+    label?: string;
+    name?: string;
+    placeHolder?: string;
+    type?: HTMLInputTypeAttribute
+    onChange?: ChangeEventHandler<HTMLTextAreaElement>
+    error?: true | undefined
 }
 

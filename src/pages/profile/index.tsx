c42b96e20cb770759af396ac12f43c89696a08d2
Medianto Pratama Pentadakosta <pentadakosta99@student.untan.ac.id>
2022-11-27 20:16:12 +0700

    profile page

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,3 +1,12 @@
-const Profile = () => {};
+import MainLayout from "../../layout/MainLayout";
+import ProfileView from "../../view/afterLogin/Profile/IndexView";
+
+const Profile = () => {
+	return (
+		<MainLayout>
+			<ProfileView />
+		</MainLayout>
+	);
+};
 
 export default Profile;

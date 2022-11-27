9ddc9ca64a92206a612c505d6064faad6c69606e
Medianto Pratama Pentadakosta <pentadakosta99@student.untan.ac.id>
2022-11-27 20:16:21 +0700

    data customer page

---
+++ src/pages/data-customer/index.tsx
@@ -0,0 +1,11 @@
+import MainLayout from "../../layout/MainLayout";
+import DataCustomerView from "../../view/afterLogin/DataCustomer/IndexView";
+
+const DataCustomer = () => {
+	return (
+		<MainLayout>
+			<DataCustomerView />
+		</MainLayout>
+	);
+};
+export default DataCustomer;

5a861804a7288cad533adf082bcc43ac77382804
Medianto Pratama Pentadakosta <out1778@fifgroup.co.id>
2023-04-03 10:25:46 +0700

    fix time out

--- src/core/api/baseApi.ts
+++ src/core/api/baseApi.ts
@@ -2,7 +2,7 @@ import { getICookies } from "../../utils/cookies/ICookies";
 import { IConstantEnum } from "../../utils/enum/IConstantEnum";
 
 
-export const timeOut = 60000 * 10
+export const timeOut = 600000
 
 export const baseUrl = () : string => {
     if ( process.env.ENV === 'prod' ) {
